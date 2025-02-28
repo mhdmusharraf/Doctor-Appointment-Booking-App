eb92be82f3822ae19f26cb0b6a924c64ba645651
mhdmusharraf <mhdmusharraf.edu@gmail.com>
2025-02-28 23:05:48 +0530

    Working on profile

--- frontend/src/pages/MyProfile.jsx
+++ frontend/src/pages/MyProfile.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {assets} from '../assets/assets'
 
 const MyProfile = () => {
+
+  const [userData,setUserData]=useState({
+    name: 'Edward Vincent',
+    image:assets.profile_pic,
+    email: 'edwardvincent@gmail.com',
+    phone: '+1 123 456 7890',
+    address:{
+      line1:'57th Cross, Richmond',
+      line2:'Circle, Church Road, London',
+    },
+    gender:'Male',
+    dob:'2000-01-01',
+  }) 
+
+  const [isEdit,setIsEdit]=useState(false)
+
   return (
     <div>
-      
+      <img src={userData.image} alt="" />
+      {
+        isEdit ?
+        <input type='text' value={userData.name} onChange={e=>setUserData(prev=>({...prev,name:e.target.value}))}/>:
+        <p>{userData.name}</p>
+      }
     </div>
   )
 }
