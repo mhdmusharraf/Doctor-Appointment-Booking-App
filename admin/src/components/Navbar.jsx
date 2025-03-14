8e916cf39748a95ddd5b707b561ba5342822dbf7
mhdmusharraf <mhdmusharraf.edu@gmail.com>
2025-03-14 23:23:30 +0530

    Working on admin navbar

---
+++ admin/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const Navbar = () => {
+  return (
+    <div>
+      <div>
+        <img src={assets.admin_logo} alt="" />
+        <p></p>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
